5315677e6a76823e19b0eb7490bdaf49f223d2f3
忘篱 <chengli.ycl@alibaba-inc.com>
2020-05-14 18:44:03 +0800

    Update script

--- trunk/scripts/stat_rtc.sh
+++ trunk/scripts/stat_rtc.sh
@@ -3,13 +3,7 @@
 files=`find src -name "*.*pp"`
 targets=""
 for file in $files; do
-  echo $file|grep -q app_rtc && targets="$targets $file" && continue;
-  echo $file|grep -q app_dtls && targets="$targets $file" && continue;
-  echo $file|grep -q app_janus && targets="$targets $file" && continue;
-  echo $file|grep -q app_rtp && targets="$targets $file" && continue;
-  echo $file|grep -q app_sdp && targets="$targets $file" && continue;
-  echo $file|grep -q kernel_rtp && targets="$targets $file" && continue;
-  echo $file|grep -q stun_stack && targets="$targets $file" && continue;
+  echo $file|grep -q _rtc && targets="$targets $file" && continue;
 done
 
 wc -l $targets;
