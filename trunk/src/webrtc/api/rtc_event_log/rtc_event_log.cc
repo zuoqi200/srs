853e3086e517e711adca0098f8ebddf5fa42e1de
winlin <winlin@vip.126.com>
2020-06-09 17:15:33 +0800

    RTC: Add example webrtc files for compiler

---
+++ trunk/src/webrtc/api/rtc_event_log/rtc_event_log.cc
@@ -0,0 +1,9 @@
+
+#include "api/rtc_event_log/rtc_event_log.h"
+#include "rtc_base/constructor_magic.h"
+
+int webrtc_compile_api()
+{
+    return 100 + webrtc_compile_base();
+}
+
---
+++ trunk/src/webrtc/api/rtc_event_log/rtc_event_log.h
@@ -0,0 +1,8 @@
+
+#ifndef WEBRTC_API_RTC_EVENT_LOG
+#define WEBRTC_API_RTC_EVENT_LOG
+
+extern int webrtc_compile_api();
+
+#endif
+
