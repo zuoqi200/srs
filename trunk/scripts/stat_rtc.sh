2ef520f1b0a6c3809bb1e29bdb7d21c559fa930c
winlin <winlin@vip.126.com>
2020-06-26 16:40:20 +0800

    Update stat code lines

--- trunk/scripts/stat_rtc.sh
+++ trunk/scripts/stat_rtc.sh
@@ -6,6 +6,9 @@ find src -name "*.*pp"|xargs wc -l|grep total
 echo "RTC of SRS"
 find src -name "*.*pp"|grep _rtc|xargs wc -l|grep total
 
+echo "ST"
+find 3rdparty/st-srs -name "*.c" -o -name "*.h" -o -name "*.S"|grep -v examples|grep -v extensions|xargs wc -l|grep total
+
 echo "WebRTC"
 find src -name "*.cc" -o -name "*.h"|xargs wc -l|grep total
 
