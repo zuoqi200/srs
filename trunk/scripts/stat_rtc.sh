5ded06a562a7e8fb337285199467e392eefdd914
winlin <winlin@vip.126.com>
2020-06-26 16:34:53 +0800

    Add stat for codes

---
+++ trunk/scripts/stat_rtc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "SRS"
+find src -name "*.*pp"|xargs wc -l|grep total
+
+echo "RTC of SRS"
+find src -name "*.*pp"|grep _rtc|xargs wc -l|grep total
+
+echo "WebRTC"
+find src -name "*.cc" -o -name "*.h"|xargs wc -l|grep total
+
+echo "FFmpeg4.2-fit"
+find 3rdparty/ffmpeg-4.2-fit -name "*.c" -o -name "*.h" -o -name "*.asm"|xargs wc -l|grep total
+
